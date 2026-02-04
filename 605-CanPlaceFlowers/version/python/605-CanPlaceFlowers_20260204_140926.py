80f2ca4caee8585f5f1571a489a433f46defd096
Vic Jeremy Prajogo <162003469+vicjeremy@users.noreply.github.com>
2026-02-04 14:09:27 +0700

    Create 605-CanPlaceFlowers

---
+++ 605-CanPlaceFlowers/version/python/605-CanPlaceFlowers_20260204_140926.py
@@ -0,0 +1,27 @@
+# Last updated: 2/4/2026, 2:09:26 PM
+1class Solution:
+2    def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
+3        can_place = 0
+4        i = 0
+5        if n <= 0:
+6            return True
+7        while i < len(flowerbed):
+8            if flowerbed[i] == 0:
+9                l_empty = i == 0 or flowerbed[i-1] == 0
+10                r_empty = i == len(flowerbed)-1  or flowerbed[i+1] == 0
+11                if l_empty and r_empty:
+12                    flowerbed[i] = 1
+13                    can_place += 1
+14                    i += 2
+15                    if can_place >= n:
+16                        return True
+17                else:
+18                    i += 3
+19            else:
+20                i += 2
+21            
+22        return False
+23
+24        
+25                
+26
